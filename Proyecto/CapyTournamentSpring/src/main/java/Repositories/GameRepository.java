0e59d81a2b130149cb983610919586e81960885b
Claudia :) <lafusarrow@gmail.com>
2022-02-19 18:30:08 +0100

    Player ent y repo

---
+++ Proyecto/CapyTournamentSpring/src/main/java/Repositories/GameRepository.java
@@ -0,0 +1,6 @@
+package Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface GameRepository extends JpaRepository<GameEntity, Long> {
+
+}
---
+++ Proyecto/CapyTournamentSpring/src/main/java/com/example/demo/GameEntity.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+public class GameEntity {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="ID_GAME", nullable=false, unique=true)
+	private Long id; 
+	
+	@Column(name="GAME_DATE", nullable=false, unique=false)
+	private LocalDate fecha;
+	
+	@Column(name="GAME_STATE", nullable=false, unique=false)
+	private String estado;
+	
+	@ManyToOne
+	private TorneoEntity torneo;
+	
+	@OneToMany
+	private List<TeamEntity> teams;
+	
+	protected GameEntity(){}
+
+	public GameEntity(Long id, LocalDate fecha, String estado, TorneoEntity torneo, List<TeamEntity> teams) {
+		super();
+		this.id = id;
+		this.fecha = fecha;
+		this.estado = estado;
+		this.torneo = torneo;
+		this.teams = teams;
+	}
+	
+	
+	
+	
+	
+}
